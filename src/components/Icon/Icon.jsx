163adc32f86938237a7960dadfffa4b33d5210b1
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-05-04 11:53:08 +0300

    add icon sprite, prettier config

---
+++ src/components/Icon/Icon.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import sprite from '../../assets/icons/icons.svg';
+
+export const Icon = ({ id, styles, ...prop }) => {
+  return (
+    <svg style={styles} {...prop}>
+      <use href={`${sprite}#${id}`} />
+    </svg>
+  );
+};
+
+Icon.propTypes = {
+  id: PropTypes.string.isRequired,
+  styles: PropTypes.object,
+  anyProp: PropTypes.any,
+};
